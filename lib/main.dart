a0707df577b8beca6eabfde92dcdc4ae2837341a
drxerxes1 <davidroyxerxesomanio@gmail.com>
2025-07-22 10:34:01 +0800

    firebase setop

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
+import 'package:attendance/firebase_options.dart';
 import 'package:attendance/helper/global.dart';
 import 'package:attendance/helper/pref.dart';
 import 'package:attendance/screen/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 
 void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   WidgetsFlutterBinding.ensureInitialized();
 
   await Pref.initialize();
