59087160cadfd903f883195971ff876e7b8f37b8
drxerxes1 <davidroyxerxesomanio@gmail.com>
2025-07-23 11:05:09 +0800

    Flutter dotenv setup

--- lib/helper/global.dart
+++ lib/helper/global.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 const appName = 'Attendance';
 
 late Size mq;
 
 const primaryColor = Color(0xFFfed22b);
+
+// firebase options
+final androidApiKey = dotenv.env['GOOGLE_API_KEY_ANDROID'].toString();
+final iosApiKey = dotenv.env['GOOGLE_API_KEY_IOS'].toString();
--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:attendance/screen/splash_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 
 void main() async {
@@ -35,6 +36,7 @@ class MyApp extends StatelessWidget {
 
 Future<void> setup() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: "assets/config/.env");
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
